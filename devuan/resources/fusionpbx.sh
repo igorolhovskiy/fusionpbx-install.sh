bce8a39a06bec3f6d021bcc01311a9aa3b9e94b5
Igor Olhovskiy <IgorOlhovskiy@gmail.com>
2020-05-21 14:11:05 +0300

    Change in all repos

--- devuan/resources/fusionpbx.sh
+++ devuan/resources/fusionpbx.sh
@@ -20,13 +20,13 @@ if [ .$system_branch = .'master' ]; then
 	verbose "Using master"
 	branch=""
 else
-	system_major=$(git ls-remote --heads https://github.com/fusionpbx/fusionpbx.git | cut -d/ -f 3 | grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f1)
-	system_minor=$(git ls-remote --tags https://github.com/fusionpbx/fusionpbx.git $system_major.* | cut -d/ -f3 |  grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f2)
+	system_major=$(git ls-remote --heads https://github.com/igorolhovskiy/fusionpbx.git | cut -d/ -f 3 | grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f1)
+	system_minor=$(git ls-remote --tags https://github.com/igorolhovskiy/fusionpbx.git $system_major.* | cut -d/ -f3 |  grep -P '^\d+\.\d+' | sort | tail -n 1 | cut -d. -f2)
 	system_version=$system_major.$system_minor
 	verbose "Using version $system_version"
 	branch="-b $system_version"
 fi
 
 #get the source code
-git clone $branch https://github.com/fusionpbx/fusionpbx.git /var/www/fusionpbx
+git clone $branch https://github.com/igorolhovskiy/fusionpbx.git /var/www/fusionpbx
 chown -R www-data:www-data /var/www/fusionpbx
